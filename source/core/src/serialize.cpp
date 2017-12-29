eebd88876ab8e206b984c25fd9d9b405595901aa
Claudio Mattera <claudio@mattera.it>
2017-12-29 13:24:57 +0100

    Add functions to serialize a blockchain

--- source/core/src/serialize.cpp
+++ source/core/src/serialize.cpp
@@ -7,45 +7,95 @@
 
 #include <naivecoin/core/utils.h>
 
+namespace {
+
+std::string format_json(Json::Value const & value)
+{
+    std::ostringstream stream;
+    stream << value;
+    return stream.str();
+}
+
+Json::Value parse_json(std::string const & text)
+{
+    // Do not use std::ostringstream, JsonCpp expect a full std::stringstream
+    std::stringstream stream(text);
+    Json::Value value;
+    stream >> value;
+    return value;
+}
+
+Json::Value serialize_block_to_json(naivecoin::Block const & block)
+{
+    Json::Value value;
+
+    value["index"] = block.index;
+    value["hash"] = block.hash;
+    value["previous_hash"] = block.previous_hash;
+    value["timestamp"] = naivecoin::format_timestamp(block.timestamp);
+    value["data"] = block.data;
+
+    return value;
+}
+
+Json::Value serialize_blockchain_to_json(std::list<naivecoin::Block> const & blockchain)
+{
+    Json::Value array(Json::arrayValue);
+
+    for (auto block: blockchain) {
+        array.append(serialize_block_to_json(block));
+    }
+
+    return array;
+}
+
+naivecoin::Block deserialize_json_to_block(Json::Value const & value)
+{
+    auto timestamp = naivecoin::parse_timestamp(value["timestamp"].asString());
+
+    naivecoin::Block block = naivecoin::Block::make_block(
+        static_cast<uint16_t>(value["index"].asInt()),
+        value["previous_hash"].asString(),
+        timestamp,
+        value["data"].asString()
+    );
+
+    if (block.hash != value["hash"].asString()) {
+        throw new std::invalid_argument("Hash mismatch");
+    }
+
+    return block;
+}
+
+} // unnamed namespace
+
 namespace naivecoin {
 
 std::string serialize_block(Block const & block)
 {
-    Json::Value root;
-
-    root["index"] = block.index;
-    root["hash"] = block.hash;
-    root["previous_hash"] = block.previous_hash;
-    root["timestamp"] = naivecoin::format_timestamp(block.timestamp);
-    root["data"] = block.data;
-
-    std::ostringstream stream;
-    stream << root;
-
-    return stream.str();
+    Json::Value const root = serialize_block_to_json(block);
+    return format_json(root);
 }
 
 std::string serialize_blockchain(std::list<Block> const & blockchain)
 {
+    Json::Value const array = serialize_blockchain_to_json(blockchain);
+    return format_json(array);
 }
 
 Block deserialize_block(std::string const & text)
 {
-    // Do not use std::ostringstream, JsonCpp expect a full std::stringstream
-    std::stringstream stream(text);
-    Json::Value root;
-    stream >> root;
-
-    auto timestamp = naivecoin::parse_timestamp(root["timestamp"].asString());
+    Json::Value const value = parse_json(text);
+    auto timestamp = naivecoin::parse_timestamp(value["timestamp"].asString());
 
     naivecoin::Block block = naivecoin::Block::make_block(
-        static_cast<uint16_t>(root["index"].asInt()),
-        root["previous_hash"].asString(),
+        static_cast<uint16_t>(value["index"].asInt()),
+        value["previous_hash"].asString(),
         timestamp,
-        root["data"].asString()
+        value["data"].asString()
     );
 
-    if (block.hash != root["hash"].asString()) {
+    if (block.hash != value["hash"].asString()) {
         throw new std::invalid_argument("Hash mismatch");
     }
 
@@ -54,6 +104,14 @@ Block deserialize_block(std::string const & text)
 
 std::list<Block> deserialize_blockchain(std::string const & text)
 {
+    Json::Value const array = parse_json(text);
+    std::list<naivecoin::Block> blockchain;
+
+    for (auto value: array) {
+        blockchain.push_back(deserialize_json_to_block(value));
+    }
+
+    return blockchain;
 }
 
 } // namespace naivecoin
--- source/core/test/serialize.cpp
+++ source/core/test/serialize.cpp
@@ -62,4 +62,98 @@ BOOST_AUTO_TEST_CASE(deserialize_block)
     BOOST_CHECK_EQUAL(block.data, data);
 }
 
+BOOST_AUTO_TEST_CASE(serialize_blockchain)
+{
+    naivecoin::Block const first_block = naivecoin::Block::make_block(
+        0,
+        "",
+        naivecoin::parse_timestamp("2017-12-28T15:00:00Z"),
+        "Some data 2\u2078 = 256"
+    );
+
+    naivecoin::Block const second_block = naivecoin::Block::make_block(
+        1,
+        "ac0c62f1871b2bda6c28af2a12f9cc1487b2d2b1",
+        naivecoin::parse_timestamp("2017-12-28T16:00:00Z"),
+        "Some other 2\u00B9\u2070 = 1024"
+    );
+
+    std::list<naivecoin::Block> const blockchain{first_block, second_block};
+
+    std::string const serialized = naivecoin::serialize_blockchain(blockchain);
+
+    std::string expected = "[\n"
+        "\t{\n"
+        "\t\t\"data\" : \"Some data 2⁸ = 256\",\n"
+        "\t\t\"hash\" : \"5153cce46e5eeea87f0e6b7998f17e356be7ffc0\",\n"
+        "\t\t\"index\" : 0,\n"
+        "\t\t\"previous_hash\" : \"\",\n"
+        "\t\t\"timestamp\" : \"2017-12-28T15:00:00Z\"\n"
+        "\t},\n"
+        "\t{\n"
+        "\t\t\"data\" : \"Some other 2¹⁰ = 1024\",\n"
+        "\t\t\"hash\" : \"b55d156f79f2085d432752b3f69021e7f82ea418\",\n"
+        "\t\t\"index\" : 1,\n"
+        "\t\t\"previous_hash\" : \"ac0c62f1871b2bda6c28af2a12f9cc1487b2d2b1\",\n"
+        "\t\t\"timestamp\" : \"2017-12-28T16:00:00Z\"\n"
+        "\t}\n"
+        "]";
+
+    BOOST_CHECK_EQUAL(serialized, expected);
+}
+
+BOOST_AUTO_TEST_CASE(deserialize_blockchain)
+{
+    std::string text = "[\n"
+        "\t{\n"
+        "\t\t\"data\" : \"Some data 2⁸ = 256\",\n"
+        "\t\t\"hash\" : \"5153cce46e5eeea87f0e6b7998f17e356be7ffc0\",\n"
+        "\t\t\"index\" : 0,\n"
+        "\t\t\"previous_hash\" : \"\",\n"
+        "\t\t\"timestamp\" : \"2017-12-28T15:00:00Z\"\n"
+        "\t},\n"
+        "\t{\n"
+        "\t\t\"data\" : \"Some other 2¹⁰ = 1024\",\n"
+        "\t\t\"hash\" : \"b55d156f79f2085d432752b3f69021e7f82ea418\",\n"
+        "\t\t\"index\" : 1,\n"
+        "\t\t\"previous_hash\" : \"ac0c62f1871b2bda6c28af2a12f9cc1487b2d2b1\",\n"
+        "\t\t\"timestamp\" : \"2017-12-28T16:00:00Z\"\n"
+        "\t}\n"
+        "]";
+
+    naivecoin::Block const first_block = naivecoin::Block::make_block(
+        0,
+        "",
+        naivecoin::parse_timestamp("2017-12-28T15:00:00Z"),
+        "Some data 2\u2078 = 256"
+    );
+
+    naivecoin::Block const second_block = naivecoin::Block::make_block(
+        1,
+        "ac0c62f1871b2bda6c28af2a12f9cc1487b2d2b1",
+        naivecoin::parse_timestamp("2017-12-28T16:00:00Z"),
+        "Some other 2\u00B9\u2070 = 1024"
+    );
+
+    std::list<naivecoin::Block> const expected_blockchain{first_block, second_block};
+
+    std::list<naivecoin::Block> const blockchain = naivecoin::deserialize_blockchain(text);
+
+    BOOST_CHECK_EQUAL(blockchain.size(), expected_blockchain.size());
+
+    auto expected_iterator = std::begin(expected_blockchain);
+    auto iterator = std::begin(blockchain);
+    for (int i = 0; i < blockchain.size(); ++i) {
+
+        BOOST_CHECK_EQUAL(iterator->index, expected_iterator->index);
+        BOOST_CHECK_EQUAL(iterator->previous_hash, expected_iterator->previous_hash);
+        BOOST_CHECK_EQUAL(iterator->hash, expected_iterator->hash);
+        BOOST_CHECK(iterator->timestamp == expected_iterator->timestamp);
+        BOOST_CHECK_EQUAL(iterator->data, expected_iterator->data);
+
+        ++expected_iterator;
+        ++iterator;
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
