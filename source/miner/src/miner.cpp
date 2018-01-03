06e397681dc700f2673f92e31d4c12f49b48adf7
Claudio Mattera <claudio@mattera.it>
2018-01-03 09:33:28 +0100

    Make miner adjust difficulty when mining new blocks

--- source/miner/src/miner.cpp
+++ source/miner/src/miner.cpp
@@ -15,8 +15,11 @@ Miner::Miner()
 void Miner::start()
 {
     while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-        std::cout << "Haha" << '\n';
+        naivecoin::Block const & latest_block = * this->blockchain.crbegin();
+        std::cout << "Latest block: " << latest_block << '\n';
+
+        //std::this_thread::sleep_for(std::chrono::seconds(10));
+        this->mine_next_block();
     }
 }
 
@@ -33,7 +36,7 @@ void Miner::mine_next_block()
     std::string const & previous_hash = latest_block.hash;
     std::string const data;
     std::time_t const timestamp= naivecoin::now();
-    uint16_t const difficulty = 0;
+    uint16_t const difficulty = this->get_difficulty();
 
     naivecoin::Block const next_block = this->find_next_block(
         index,
@@ -76,6 +79,48 @@ naivecoin::Block Miner::find_next_block(
         } else {
             ++nonce;
         }
+    }
+}
+
+uint16_t Miner::get_difficulty()
+{
+    naivecoin::Block const & latest_block = * this->blockchain.crbegin();
+    if (latest_block.index % Miner::DIFFICULTY_ADJUSTMENT_INTERVAL_IN_BLOCKS == 0) {
+        return this->get_adjusted_difficulty(latest_block);
+    } else {
+        return latest_block.difficulty;
+    }
+}
+
+uint16_t Miner::get_adjusted_difficulty(naivecoin::Block const & latest_block)
+{
+    std::cout << "Adjusting difficulty for block " << latest_block.index;
+    auto iterator = this->blockchain.crbegin();
+    for (int i = 0; i < Miner::DIFFICULTY_ADJUSTMENT_INTERVAL_IN_BLOCKS; ++i) {
+        ++iterator;
+    }
+    naivecoin::Block const latest_adjustment_block = * iterator;
+
+    auto expected_elapsed_time = std::chrono::seconds(
+        Miner::DIFFICULTY_ADJUSTMENT_INTERVAL_IN_BLOCKS * Miner::BLOCK_GENERATION_INTERVAL_IN_SECONDS
+    );
+
+    auto actual_elapsed_time = std::chrono::seconds(
+        static_cast<int>(
+            std::difftime(latest_block.timestamp, latest_adjustment_block.timestamp)
+        )
+    );
+
+    std::cout << ", elapsed time: " << actual_elapsed_time.count() << "s";
+    if (actual_elapsed_time < expected_elapsed_time / 2) {
+        std::cout << ", increasing difficulty" << '\n';
+        return latest_block.difficulty + 1;
+    } else if (actual_elapsed_time > expected_elapsed_time * 2) {
+        std::cout << ", decreasing difficulty" << '\n';
+        return latest_block.difficulty - 1;
+    } else {
+        std::cout << ", maintaining difficulty" << '\n';
+        return latest_block.difficulty;
     }
 }
 
--- source/miner/src/miner.h
+++ source/miner/src/miner.h
@@ -36,6 +36,10 @@ private:
         MINE_NEXT_BLOCK
     };
 
+    uint16_t const BLOCK_GENERATION_INTERVAL_IN_SECONDS = 10;
+
+    uint16_t const DIFFICULTY_ADJUSTMENT_INTERVAL_IN_BLOCKS = 10;
+
 private:
     void mine_next_block();
     naivecoin::Block find_next_block(
@@ -45,6 +49,8 @@ private:
         std::string const & data,
         uint16_t const difficulty
     );
+    uint16_t get_difficulty();
+    uint16_t get_adjusted_difficulty(naivecoin::Block const & latest_block);
 
 private:
     std::list<naivecoin::Block> blockchain;
