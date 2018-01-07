2ad6c2b8f32d83fb0a4389e72066752c745f22d9
Claudio Mattera <claudio@mattera.it>
2018-01-07 21:36:10 +0100

    Move template code to .cpp file and use explicit instantiation

--- source/core/include/naivecoin/core/utils.h
+++ source/core/include/naivecoin/core/utils.h
@@ -15,25 +15,7 @@ std::time_t parse_timestamp(std::string const & text);
 std::string format_timestamp(std::time_t const & timestamp);
 
 template <int N>
-std::array<bool, 4 * N> hex_to_binary(std::string const & text)
-{
-    std::array<bool, 4 * N> binary;
-
-    for (int i = 0; i < N; ++i) {
-        char const digit = text[i];
-        std::array<char, 2> const s{digit, 0};
-        uint8_t number = std::strtoul(s.data(), nullptr, 16);
-        binary[4*i + 3] = number % 2;
-        number /= 2;
-        binary[4*i + 2] = number % 2;
-        number /= 2;
-        binary[4*i + 1] = number % 2;
-        number /= 2;
-        binary[4*i] = number % 2;
-    }
-
-    return binary;
-}
+std::array<bool, 4 * N> hex_to_binary(std::string const & text);
 
 } // namespace naivecoin
 
--- source/core/src/utils.cpp
+++ source/core/src/utils.cpp
@@ -25,4 +25,38 @@ std::string format_timestamp(std::time_t const & timestamp)
     return stream.str();
 }
 
+template <int N>
+std::array<bool, 4 * N> hex_to_binary(std::string const & text)
+{
+    std::array<bool, 4 * N> binary;
+
+    for (int i = 0; i < N; ++i) {
+        char const digit = text[i];
+        std::array<char, 2> const s{digit, 0};
+        uint8_t number = std::strtoul(s.data(), nullptr, 16);
+        binary[4*i + 3] = number % 2;
+        number /= 2;
+        binary[4*i + 2] = number % 2;
+        number /= 2;
+        binary[4*i + 1] = number % 2;
+        number /= 2;
+        binary[4*i] = number % 2;
+    }
+
+    return binary;
+}
+
+
+// Explicit template instantiations
+
+// For SHA1
+template
+std::array<bool, 4 * 20> hex_to_binary<20>(std::string const & text);
+
+// For tests
+template
+std::array<bool, 4 * 1> hex_to_binary<1>(std::string const & text);
+template
+std::array<bool, 4 * 8> hex_to_binary<8>(std::string const & text);
+
 } // namespace naivecoin
