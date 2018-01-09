5ca3dfb828728f70614ca0f49cec36b841bfb7e0
Claudio Mattera <claudio@mattera.it>
2018-01-09 22:10:43 +0100

    Improve HTTP library to pass content lengths

--- source/http/src/http.cpp
+++ source/http/src/http.cpp
@@ -5,9 +5,23 @@
 
 namespace {
 
-std::string make_response(std::string const & code, std::string const & data)
+std::string make_response(
+	uint16_t const & code,
+	std::string const & english,
+	std::string const & data,
+	uint16_t const http_version = 0
+)
 {
-	return code + std::string("\r\n\r\n") + data;
+	std::ostringstream stream;
+	stream << "HTTP/1." << http_version << " " << code << " " << english << "\r\n";
+
+	if (! data.empty()) {
+		stream << "Content-Length: " << data.size() << "\r\n";
+		stream << "\r\n";
+		stream << data;
+	}
+
+	return stream.str();
 }
 
 } // unnamed namespace
@@ -23,25 +37,44 @@ std::string process_request(
 		std::size_t const first_newline_pos = request.find("\r\n");
 		std::string const & first_line = request.substr(0, first_newline_pos);
 
-		std::regex const first_line_re{"^(GET|POST) (\\S+) HTTP/1.0$"};
+		std::regex const first_line_re{"^(GET|POST) (\\S+) HTTP/1.(0|1)$"};
 		std::smatch first_line_match;
 		if (! std::regex_match(first_line, first_line_match, first_line_re)) {
-			return make_response("HTTP/1.0 400 Bad request", "Invalid HTTP request");
+			return make_response(400, "Bad request", "Invalid HTTP request");
 		}
+
+		uint16_t const http_version = std::strtoul(first_line_match[3].str().c_str(), nullptr, 10);
 
 		Method const method = first_line_match[1] == "GET" ? GET : POST;
 		std::string const path = first_line_match[2];
+
+		std::size_t expected_length = std::string::npos;  // By default read everything
+
+		std::regex const header_line_re{"^([^:]+): (.+?)\r\n"};
+		std::smatch header_line_match;
+		std::string header_line = request.substr(2 + first_line_match.position() + first_line_match.length());
+		while (std::regex_search(header_line, header_line_match, header_line_re)) {
+			std::string const & header = header_line_match[1];
+			std::string const & value = header_line_match[2];
+
+			if (header == "Content-Length") {
+				expected_length = std::strtoul(value.c_str(), nullptr, 10);
+			}
+
+			header_line = header_line_match.suffix();
+		}
+
 		std::size_t const start = 4 + request.find("\r\n\r\n");
-		std::string const data = request.substr(start);
+		std::string const data = request.substr(start, expected_length);
 
 		try {
 			std::string const response_data = callback(method, path, data);
-			return make_response("HTTP/1.0 200 Ok", response_data);
+			return make_response(200, "Ok", response_data, http_version);
 		} catch (std::exception const & e) {
-			return make_response("HTTP/1.0 400 Bad request", e.what());
+			return make_response(400, "Bad request", e.what(), http_version);
 		}
 	} catch (std::exception const & e) {
-		return make_response("HTTP/1.0 500 Server error", "");
+		return make_response(500, "Server error", "");
 	}
 }
 
--- source/http/test/http.cpp
+++ source/http/test/http.cpp
@@ -7,31 +7,86 @@
 
 BOOST_AUTO_TEST_SUITE(HttpHttp)
 
-BOOST_AUTO_TEST_CASE(compute_hash)
+BOOST_AUTO_TEST_CASE(process_request)
 {
-	std::string const request = "GET /my/path HTTP/1.0\r\n"
-		"\r\n"
-		"My data";
+    std::string const request = "GET /my/path HTTP/1.0\r\n"
+        "\r\n"
+        "My data";
 
-	naivecoin::Method outer_method;
-	std::string outer_path;
-	std::string outer_data;
+    naivecoin::Method outer_method;
+    std::string outer_path;
+    std::string outer_data;
 
-	std::string const response = naivecoin::process_request(
-		request,
-		[&](naivecoin::Method const method, std::string const & path, std::string const & data) {
-			outer_method = method;
-			outer_path = path;
-			outer_data = data;
-			return "My response";
-		}
-	);
+    std::string const response = naivecoin::process_request(
+        request,
+        [&](naivecoin::Method const method, std::string const & path, std::string const & data) {
+            outer_method = method;
+            outer_path = path;
+            outer_data = data;
+            return "My response";
+        }
+    );
 
     BOOST_CHECK_EQUAL(outer_method, naivecoin::GET);
     BOOST_CHECK_EQUAL(outer_path, "/my/path");
     BOOST_CHECK_EQUAL(outer_data, "My data");
 
-    BOOST_CHECK_EQUAL(response, "HTTP/1.0 200 Ok\r\n\r\nMy response");
+    BOOST_CHECK_EQUAL(response, "HTTP/1.0 200 Ok\r\nContent-Length: 11\r\n\r\nMy response");
+}
+
+BOOST_AUTO_TEST_CASE(process_request_http11)
+{
+    std::string const request = "GET /my/path HTTP/1.1\r\n"
+        "\r\n"
+        "My data";
+
+    naivecoin::Method outer_method;
+    std::string outer_path;
+    std::string outer_data;
+
+    std::string const response = naivecoin::process_request(
+        request,
+        [&](naivecoin::Method const method, std::string const & path, std::string const & data) {
+            outer_method = method;
+            outer_path = path;
+            outer_data = data;
+            return "My response";
+        }
+    );
+
+    BOOST_CHECK_EQUAL(outer_method, naivecoin::GET);
+    BOOST_CHECK_EQUAL(outer_path, "/my/path");
+    BOOST_CHECK_EQUAL(outer_data, "My data");
+
+    BOOST_CHECK_EQUAL(response, "HTTP/1.1 200 Ok\r\nContent-Length: 11\r\n\r\nMy response");
+}
+
+BOOST_AUTO_TEST_CASE(process_request_explicit_length)
+{
+    std::string const request = "GET /my/path HTTP/1.1\r\n"
+        "Content-Length: 7\r\n"
+        "\r\n"
+        "My data and garbage";
+
+    naivecoin::Method outer_method;
+    std::string outer_path;
+    std::string outer_data;
+
+    std::string const response = naivecoin::process_request(
+        request,
+        [&](naivecoin::Method const method, std::string const & path, std::string const & data) {
+            outer_method = method;
+            outer_path = path;
+            outer_data = data;
+            return "My response";
+        }
+    );
+
+    BOOST_CHECK_EQUAL(outer_method, naivecoin::GET);
+    BOOST_CHECK_EQUAL(outer_path, "/my/path");
+    BOOST_CHECK_EQUAL(outer_data, "My data");
+
+    BOOST_CHECK_EQUAL(response, "HTTP/1.1 200 Ok\r\nContent-Length: 11\r\n\r\nMy response");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
